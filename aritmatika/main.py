6b17a49819af8ed63bbfa66bc1e32cc20b982abc
Nugie kurniawan <nugiebinhar@gmail.com>
2025-08-12 20:01:24 +0700

    ulang operasi aritmatika

---
+++ aritmatika/main.py
@@ -0,0 +1,32 @@
+# macam macam operasi aritmatika
+
+# pertambahan
+a = 10
+b = 3
+
+hasil = a+b
+print(f"{a} + {b} = {hasil}")
+
+# pengurangan
+hasil = a-b
+print(f"{a} - {b} = {hasil}")
+
+# perkalian
+hasil = a*b
+print(f"{a} * {b} = {hasil}")
+
+# pembagian
+hasil = a/b
+print(f"{a} / {b} = {hasil}")
+
+# modulus
+hasil = a%b
+print(f"{a} % {b} = {hasil}")
+
+# exponen
+hasil = a**b
+print(f"{a} ** {b} = {hasil}")
+
+# floor division
+hasil = a//b
+print(f"{a} // {b} = {hasil}")
