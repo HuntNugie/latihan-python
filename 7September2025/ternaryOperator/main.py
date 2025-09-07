25bde9da86fb084adb2989429b0791a3de0554af
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-07 22:25:56 +0700

    ternary opertor

---
+++ 7September2025/ternaryOperator/main.py
@@ -0,0 +1,6 @@
+# ternary operator merupakan berfungsi untuk mempersingkat kode if jika hanya memiliki 2 kondisi kalau ga benar ya salah atau istilah nya bentuk sederhana dari if else
+
+inputan = int(input("Masukan angka : "))
+
+hasil = "genap" if inputan %  2==0 else "ganjil"
+print(hasil)
