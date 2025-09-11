6a6272562da4fe0500ff55dbb94b4016177b225e
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-11 22:18:08 +0700

    while looping

---
+++ 11September2025/while/main.py
@@ -0,0 +1,11 @@
+# while merupakan salah satu dari caralooping dimana akan mengulang trus program yang ada di dalam nya selama kondisi nya masih true
+i = 0
+while i<5:
+    print("Hello world")
+    i+=1
+    
+ulang = int(input("Mau ulang nama berapa kali : "))
+index = 0
+while index < ulang:
+    print("Nugie kurniawan")
+    index += 1
